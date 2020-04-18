27b2648da467eabc6e519ead331a9e61de2662b9
macaos <macaoshu@gmail.com>
2020-04-19 01:37:33 +0900

    SVG Path Info Extraction

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const targetFolder = "./SVG";
 const fs = require("fs");
+const prefix = "themify-";
+let outputStr = "";
 
 // read folder
 fs.readdir(targetFolder, function(error, filelist) {
-  filelist.forEach(item => readFileBody(item));
+  filelist.forEach(item => {
+    // print Type
+    const name = prefix + item.replace(/.svg/, "");
+    addStr(`"${name}":string,`);
+    setTimeout(() => {
+      // print storybook
+      addStr(`{ name: "${name}", size: "20px", viewBoxWidth: "17" },`);
+    }, 500);
+
+    readFileBody(item);
+  });
+  setTimeout(() => {
+    // addStr
+    fs.writeFile("exportIconInfo.js", outputStr, "utf-8", () => {
+      console.log("saved!");
+    });
+  }, 2000);
 });
 
 // read file
-function readFileBody(path) {
-  console.log(path);
-  fs.readFile(targetFolder + `/${path}`, "utf8", function(err, data) {
-    console.log(data);
+function readFileBody(fileName) {
+  fs.readFile(targetFolder + `/${fileName}`, "utf8", function(err, data) {
+    let svgpath = data.match(/<path d="[^"]+"/g);
+    svgpath = String(svgpath).replace(/<path d="/, '"');
+    const file = fileName.replace(/.svg/, "");
+    // print key(name):value(path)
+    addStr(`"${prefix}${file}":${svgpath},`);
   });
+}
+
+function addStr(str) {
+  outputStr += str + "\n";
 }
